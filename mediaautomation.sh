ea120b5591e58119bb71624ed617993eefd91b48
Micheal Howlin <micheal.howlin@jacobstechnology.co.uk>
2022-04-13 20:39:55 +0100

    Working Server Install

--- mediaautomation.sh
+++ mediaautomation.sh
@@ -28,11 +28,10 @@ SAMBANAME="mediaautomation"
 QBITTORRENTPORT="8080"
 IP=$(hostname -I)
 IP=$(echo $IP | xargs)
-#IP="127.0.0.1"
 ARCH=$(dpkg --print-architecture)
-#set -o allexport
-#source "config.env"
-#set +o allexport
+
+#Hardcode your IP here
+#IP="127.0.0.1"
 
 ################################################################################
 # Help                                                                         #
@@ -67,14 +66,16 @@ done
 ################################################################################
 
 structure(){
-  echo "Step 1 - Creating Folder Structure and Updating";
+  echo "################################################################################";
+  echo "# Step 1 - Creating Folder Structure and Updating                              #";
+  echo "################################################################################";
   sudo mkdir -p $DATAFOLDER
   sudo mkdir -p "$DATAFOLDER/torrents/downloading"
   sudo mkdir -p "$DATAFOLDER/torrents/complete"
   sudo mkdir -p "$DATAFOLDER/video/movies"
   sudo mkdir -p "$DATAFOLDER/video/tv"
   sudo apt update
-  sudo apt-get install -y curl wget zip unzip git
+  sudo apt-get install -y curl wget zip unzip git apt-transport-https
   sudo ufw allow 80
   sudo ufw allow 32400
   sudo ufw allow 7878
@@ -84,9 +85,13 @@ structure(){
   sudo ufw allow 8080
 }
 structure-uninstall(){
-  echo "Removing Folder Structure";
+  echo "################################################################################";
+  echo "# Removing Folder Structure                                                    #";
+  echo "################################################################################";
   sudo rm -rf $DATAFOLDER
-  echo "Closing Firewall Ports"
+  echo "################################################################################";
+  echo "# Closing Firewall Ports                                                       #";
+  echo "################################################################################";
   sudo ufw deny 80
   sudo ufw deny 32400
   sudo ufw deny 7878
@@ -97,7 +102,9 @@ structure-uninstall(){
 }
 
 cifs(){
-  echo "Step 2 - Cifs Utils and Samba"
+  echo "################################################################################";
+  echo "# Step 2 - Cifs Utils and Samba                                                #";
+  echo "################################################################################";
   sudo apt install cifs-utils -y
   sudo apt install samba -y
   echo "[$SAMBANAME]
@@ -111,35 +118,53 @@ cifs(){
   read only = no" | sudo tee -a /etc/samba/smb.conf
 }
 cifs-uninstall(){
-  echo "Removing Lines From Samba"
+  echo "################################################################################";
+  echo "# Removing Lines From Samba                                                    #";
+  echo "################################################################################";
   sudo sed -i "$(( $(wc -l </etc/samba/smb.conf)-9+1 )),$ d" /etc/samba/smb.conf
   sudo service smbd restart
 }
 
 plex(){
-  echo "Step 3 - Installing Plex Media Server"
+  echo "################################################################################";
+  echo "# Step 3 - Installing Plex Media Server                                        #";
+  echo "################################################################################";
   curl https://downloads.plex.tv/plex-keys/PlexSign.key | sudo apt-key add -
   echo deb https://downloads.plex.tv/repo/deb public main | sudo tee /etc/apt/sources.list.d/plexmediaserver.list
   sudo apt update
   sudo apt install plexmediaserver -y
 }
 plex-uninstall(){
-  echo "Uninstalling Plex Media Server"
+  echo "################################################################################";
+  echo "# Uninstalling Plex Media Server                                               #";
+  echo "################################################################################";
   sudo apt remove plexmediaserver -y
   sudo rm /etc/apt/sources.list.d/plexmediaserver.list
 }
 
 radarr(){
-  echo "Step 4 - Installing Radarr"
+  echo "################################################################################";
+  echo "# Step 4 - Installing Radarr                                                   #";
+  echo "################################################################################";
   sudo mkdir -p /home/radarr/
   sudo chmod -R 777 /home/radarr/
   sudo addgroup media && sudo adduser --system --no-create-home radarr --ingroup media
   sudo apt install curl sqlite3 -y
-  sudo wget -nc --content-disposition 'http://radarr.servarr.com/v1/update/master/updatefile?os=linux&runtime=netcore&arch=x64'
+
+  if [ "$ARCH" = "amd64" ];
+    then
+      sudo wget -nc --content-disposition 'http://radarr.servarr.com/v1/update/master/updatefile?os=linux&runtime=netcore&arch=x64'
+  elif [ "$ARCH" = "arm64" ];
+    then
+      sudo wget -nc --content-disposition 'http://radarr.servarr.com/v1/update/master/updatefile?os=linux&runtime=netcore&arch=arm64'
+  else
+    sudo wget -nc --content-disposition 'http://radarr.servarr.com/v1/update/master/updatefile?os=linux&runtime=netcore&arch=arm'
+  fi
+
   sudo tar -xvzf Radarr*.linux*.tar.gz
   sudo mv Radarr /opt/
   sudo chown radarr:media -R /opt/Radarr
-
+  sudo rm -rf Radarr*.linux*.tar.gz
   sudo touch /etc/systemd/system/radarr.service
   echo "[Unit]
 Description=Radarr Daemon
@@ -158,10 +183,11 @@ WantedBy=multi-user.target" | sudo tee -a /etc/systemd/system/radarr.service
   sudo systemctl -q daemon-reload
   sudo systemctl enable radarr.service
   sudo systemctl start radarr.service
-  #rm Radarr*.linux*.tar.gz
 }
 radarr-uninstall(){
-  echo "Uninstalling Radarr"
+  echo "################################################################################";
+  echo "# Uninstalling Radarr                                                          #";
+  echo "################################################################################";
   sudo systemctl stop radarr.service
   sudo rm -rf /opt/Radarr
   sudo rm -rf /etc/systemd/system/radarr.service
@@ -171,19 +197,36 @@ radarr-uninstall(){
 }
 
 sonarr(){
-  echo "Step 5 - Installing Sonarr"
-  sudo apt install gnupg ca-certificates
-  sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-  echo "deb https://download.mono-project.com/repo/ubuntu stable-focal main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
+  echo "################################################################################";
+  echo "# Step 5 - Installing Sonarr                                                   #";
+  echo "################################################################################";
+  sudo apt install gnupg ca-certificates dirmngr
   wget -nc https://mediaarea.net/repo/deb/repo-mediaarea_1.0-19_all.deb && sudo dpkg -i repo-mediaarea_1.0-19_all.deb
-  sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 2009837CBFFD68F45BC180471F4F90DE2A9B4BF8
-  echo "deb https://apt.sonarr.tv/ubuntu focal main" | sudo tee /etc/apt/sources.list.d/sonarr.list
+
+  if [ "$ARCH" = "amd64" ];
+    then
+      sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+      echo "deb https://download.mono-project.com/repo/ubuntu stable-focal main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
+
+      sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 2009837CBFFD68F45BC180471F4F90DE2A9B4BF8
+      echo "deb https://apt.sonarr.tv/ubuntu focal main" | sudo tee /etc/apt/sources.list.d/sonarr.list
+  else
+    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+    echo "deb https://download.mono-project.com/repo/debian stable-raspbianbuster main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
+
+    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 2009837CBFFD68F45BC180471F4F90DE2A9B4BF8
+    echo "deb https://apt.sonarr.tv/debian buster main" | sudo tee /etc/apt/sources.list.d/sonarr.list
+  fi
+
   sudo apt update
   sudo apt install sonarr -y
   sudo cert-sync /etc/ssl/certs/ca-certificates.crt
+  sudo rm -rf repo-mediaarea*
 }
 sonarr-uninstall(){
-  echo "Uninstalling Sonarr"
+  echo "################################################################################";
+  echo "# Uninstalling Sonarr                                                          #";
+  echo "################################################################################";
   sudo apt remove sonarr -y
   sudo rm /etc/apt/sources.list.d/mono-official-stable.list
   sudo rm /etc/apt/sources.list.d/sonarr.list
@@ -191,11 +234,21 @@ sonarr-uninstall(){
 }
 
 jackett(){
-  echo "Step 6 - Installing Jackett"
-  cd /opt && f=Jackett.Binaries.LinuxAMDx64.tar.gz && release=$(wget -q https://github.com/Jackett/Jackett/releases/latest -O - | grep "title>Release" | cut -d " " -f 4) && sudo wget -Nc https://github.com/Jackett/Jackett/releases/download/$release/"$f" && sudo tar -xzf "$f" && sudo rm -f "$f" && cd Jackett* && sudo ./install_service_systemd.sh
+  echo "################################################################################";
+  echo "# Step 6 - Installing Jackett                                                  #";
+  echo "################################################################################";
+
+  if [ "$ARCH" = "amd64" ];
+      then
+        cd /opt && f=Jackett.Binaries.LinuxAMDx64.tar.gz && release=$(wget -q https://github.com/Jackett/Jackett/releases/latest -O - | grep "title>Release" | cut -d " " -f 4) && sudo wget -Nc https://github.com/Jackett/Jackett/releases/download/$release/"$f" && sudo tar -xzf "$f" && sudo rm -f "$f" && cd Jackett* && sudo ./install_service_systemd.sh
+    else
+      cd /opt && f=Jackett.Binaries.LinuxARM32.tar.gz && release=$(wget -q https://github.com/Jackett/Jackett/releases/latest -O - | grep "title>Release" | cut -d " " -f 4) && sudo wget -Nc https://github.com/Jackett/Jackett/releases/download/$release/"$f" && sudo tar -xzf "$f" && sudo rm -f "$f" && cd Jackett* && sudo ./install_service_systemd.sh
+  fi
 }
 jackett-uninstall(){
-  echo "Uninstalling Jackett"
+  echo "################################################################################";
+  echo "# Uninstalling Jackett                                                         #";
+  echo "################################################################################";
   sudo systemctl stop jackett.service
   sudo rm /etc/systemd/system/jackett.service
   sudo rm -rf ~/.config/Jackett
@@ -203,7 +256,9 @@ jackett-uninstall(){
 }
 
 qbittorrent(){
-  echo "Step 7 - Installing Qbittorrent"
+  echo "################################################################################";
+  echo "# Step 7 - Installing Qbittorrent                                              #";
+  echo "################################################################################";
   sudo add-apt-repository -y ppa:qbittorrent-team/qbittorrent-stable
   sudo apt install qbittorrent-nox -y
   sudo touch /etc/systemd/system/qbittorrent-nox.service
@@ -221,14 +276,18 @@ WantedBy=multi-user.target" | sudo tee -a /etc/systemd/system/qbittorrent-nox.se
   sudo systemctl start qbittorrent-nox.service
 }
 qbittorrent-uninstall(){
-  echo "Uninstalling Qbittorrent"
+  echo "################################################################################";
+  echo "# Uninstalling Qbittorrent                                                     #";
+  echo "################################################################################";
   sudo systemctl stop qbittorrent-nox.service
   sudo apt remove qbittorrent-nox -y
   sudo rm /etc/systemd/system/qbittorrent-nox.service
 }
 
 tautilli(){
-  echo "Step 8 - Installing Tautilli"
+  echo "################################################################################";
+  echo "# Step 8 - Installing Tautilli                                                 #";
+  echo "################################################################################";
   sudo apt-get install git python3.7 python3-setuptools -y
   cd /opt
   sudo git clone https://github.com/Tautulli/Tautulli.git
@@ -239,7 +298,9 @@ tautilli(){
   sudo systemctl start tautulli.service
 }
 tautilli-uninstall(){
-  echo "Uninstalling Tautilli"
+  echo "################################################################################";
+  echo "# Uninstalling Tautilli                                                        #";
+  echo "################################################################################";
   sudo systemctl stop tautulli.service
   sudo rm /lib/systemd/system/tautulli.service
   sudo rm -rf /opt/Tautulli
@@ -247,16 +308,22 @@ tautilli-uninstall(){
 }
 
 openpyn(){
-  echo "Step 9 - Installing OpenPyn"
+  echo "################################################################################";
+  echo "# Step 9 - Installing OpenPyn                                                  #";
+  echo "################################################################################";
   sudo python3 -m pip install --upgrade openpyn
 }
 openpyn-uninstall(){
-  echo "Uninstalling Openpyn"
+  echo "################################################################################";
+  echo "# Uninstalling Openpyn                                                         #";
+  echo "################################################################################";
   sudo python3 -m pip uninstall openpyn
 }
 
 simpledash(){
-  echo "Step 10 - Installing SimpleDash"
+  echo "################################################################################";
+  echo "# Step 10 - Installing SimpleDash                                              #";
+  echo "################################################################################";
   sudo apt-get install -y nginx
   sudo systemctl enable nginx.service
   sudo rm -rf /var/www/html
@@ -294,29 +361,48 @@ simpledash(){
           		{
           			"alt" : "Qbittorrent",
           			"icon" : "fa fa-tint",
-          			"link" : "http://'"$IP"':8080/"
+          			"link" : "http://'"$IP"':'"$QBITTORRENTPORT"'/"
           		}
           	]
           }' | sudo tee -a /var/www/html/config.json
 }
 simpledash-uninstall(){
-  echo "Uninstalling Simpledash"
+  echo "################################################################################";
+  echo "# Uninstalling Simpledash                                                      #";
+  echo "################################################################################";
   sudo rm -rf /var/www/html
   sudo apt remove nginx -y
+}
+
+verify(){
+  echo "################################################################################";
+  echo "# Verifying Install                                                            #";
+  echo "################################################################################";
+  systemctl is-active --quiet smbd
+  systemctl is-active --quiet radarr
+  systemctl is-active --quiet sonarr
+  systemctl is-active --quiet jackett
+  systemctl is-active --quiet qbittorrent-nox
+  systemctl is-active --quiet plexmediaserver
+  systemctl is-active --quiet tautulli
+  systemctl is-active --quiet nginx
+  echo "################################################################################";
+  echo "# Verified - All Services Running Correctly                                    #";
+  echo "################################################################################";
 }
 
 install(){
   structure
   cifs
   plex
-  qbittorrent
-  jackett
   radarr
   sonarr
+  jackett
+  qbittorrent
   tautilli
   openpyn
   simpledash
-  echo "Installation Complete Visit http://$IP";
+  verify
 }
 uninstall(){
   simpledash-uninstall
@@ -330,7 +416,6 @@ uninstall(){
   tautilli-uninstall
   structure-uninstall
   sudo groupdel media
-  echo "Uninstalled Completely"
 }
 
 if [[ $1 == "install" ]];
@@ -347,6 +432,9 @@ if [[ $1 == "install" ]];
         esac
     done
     install;
+    echo "################################################################################";
+    echo "# Installation Complete Visit http://$IP                                       #";
+    echo "################################################################################";
 
 elif [[ $1 == "uninstall" ]];
   then
@@ -354,6 +442,9 @@ elif [[ $1 == "uninstall" ]];
     echo "# Uninstalling                                                                 #";
     echo "################################################################################";
     uninstall;
+    echo "################################################################################";
+    echo "# Uninstalled Completely                                                       #";
+    echo "################################################################################";
 
 else
   echo "$1 was not recognised"
